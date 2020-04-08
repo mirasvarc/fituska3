55878844876d18ab4fa6540bd66119464bca2a67
Miroslav Švarc <xsvarc06@vutbr.cz>
2020-04-08 16:41:32 +0200

    add app name

--- src/routes/web.php
+++ src/routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get('/app', function () {
+    return view('layouts/app');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
