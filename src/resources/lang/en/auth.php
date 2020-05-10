a944ad1dec738eb47dec7be9a95df3882351ed46
Miroslav Švarc <mirasvarc1@gmail.com>
2020-05-10 16:42:26 +0200

    translate failed login

--- src/resources/lang/en/auth.php
+++ src/resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Špatné uživatelské jméno nebo heslo.',
+    'throttle' => 'Příliž mnoho pokusů o přihlášení. Zkuste prosim znovu za :seconds sekund.',
 
 ];
