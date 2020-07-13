0768e0dc732756394de9aed58d14d82e380dd6b2
Miroslav Švarc <mirasvarc1@gmail.com>
2020-07-13 23:06:04 +0200

    add models

--- src/app/HasRole.php
+++ src/app/HasRole.php
@@ -4,8 +4,15 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class has_role extends Model
+class HasRole extends Model
 {
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
     protected $table = 'has_role';
+
+
     public $foreignKey = 'user_id';
 }
