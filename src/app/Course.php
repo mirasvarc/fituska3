7f4682b2563e14c1b7e9c90f5d42bc1199da47d9
Miroslav Švarc <mirasvarc1@gmail.com>
2020-08-22 16:47:55 +0200

    add core functions

--- src/app/Course.php
+++ src/app/Course.php
@@ -3,8 +3,31 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\User;
+use App\IsFollowingCourse;
 class Course extends Model
 {
-    //
+
+    /**
+     * get all courses from db
+     */
+    public static function getAllCourses(){
+        return Course::all();
+    }
+
+    /**
+     * get course by ID
+     * @param id course id
+     */
+    public static function getCourse($id){
+        return Course::find($id);
+    }
+
+    /**
+     * get followed courses for specified user
+     * @param user_id user id
+     */
+    public static function getFollowedCourses($user_id) {
+        return User::find($user_id)->followedCourses()->get();
+    }
 }
